94c6c4ccc658b53a4b6c0a08dec8faeaa32ee085
Adrián Maza <adri_mv1@hotmail.com>
2025-05-20 10:57:08 +0200

    vue project created

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HeaderComponent from './components/HeaderComponent.vue';
+import InputComponent from './components/InputComponent.vue';
+import VariablesComponent from './components/VariablesComponent.vue';
+</script>
+
+<template>
+  <HeaderComponent />
+
+  <h2>Hola a todos!</h2>
+
+  <VariablesComponent />
+  <InputComponent />
+</template>
+
+<style></style>
---
+++ src/components/VariablesComponent.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from "vue";
+
+const counter = ref(0);
+const counter2 = ref(0);
+const message = ref("Esto es un mensaje");
+
+setInterval(() => {
+  counter.value = counter.value + 1;
+}, 1000);
+
+function changeMsg() {
+  message.value = "Ahora es otro mensaje";
+}
+
+function modifyCounter2(number) {
+  counter2.value = counter2.value + number;
+}
+</script>
+
+<template>
+  <h2>Counter: {{ counter }}</h2>
+  <h2>
+    Counter2:
+    <button @click="modifyCounter2(-1)">-</button>
+    {{ counter2 }}
+    <button @click="modifyCounter2(1)">+</button>
+  </h2>
+  <p>{{ message }}</p>
+  <button @click="changeMsg">Cambiar Mensaje</button>
+</template>
+
+<style></style>
